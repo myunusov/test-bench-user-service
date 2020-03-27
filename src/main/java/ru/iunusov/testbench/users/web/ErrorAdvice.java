5677a3cfea11c1c52fb33110ab4f7ab83e8683f6
Maxim Iunusov <maxim@iunusov.ru>
2020-03-27 13:45:05 +0300

    Add errors description (OpenAPI 3)

--- src/main/java/ru/iunusov/testbench/users/web/ErrorAdvice.java
+++ src/main/java/ru/iunusov/testbench/users/web/ErrorAdvice.java
@@ -1,5 +1,7 @@
 package ru.iunusov.testbench.users.web;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
@@ -15,6 +17,7 @@ public class ErrorAdvice {
 
   @ExceptionHandler(RuntimeException.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ApiResponses(value = { @ApiResponse(responseCode = "500", description = "Internal server error") })
   public ResponseEntity<String> internalServerError(@NotNull final RuntimeException e) {
     log.error("Internal server error", e);
     return new ResponseEntity<>(
@@ -23,12 +26,14 @@ public class ErrorAdvice {
 
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ApiResponses(value = { @ApiResponse(responseCode = "400", description = "Bad request") })
   public ResponseEntity<String> badRequest(@NotNull final IllegalArgumentException e) {
     return new ResponseEntity<>("Bad request: " + e.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(NotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ApiResponses(value = { @ApiResponse(responseCode = "404", description = "Resource not found") })
   public ResponseEntity<String> notFoundError(@NotNull final NotFoundException e) {
     return new ResponseEntity<>("Not found error: " + e.getMessage(), HttpStatus.NOT_FOUND);
   }
